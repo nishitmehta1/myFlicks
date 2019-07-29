95819766ffe40304fc4825d5be70a54c3f3b4945
Nishit Mehta <nishit.mehta1@gmail.com>
2019-07-28 20:33:05 -0500

    Fix search feature

--- client/src/App.js
+++ client/src/App.js
@@ -1,13 +1,13 @@
-import React, { Component } from "react";
-import "./App.css";
+import React, { Component } from 'react';
+import './App.css';
 // import 'bootstrap/dist/css/bootstrap.min.css';
-import "font-awesome/css/font-awesome.min.css";
-import { BrowserRouter as Router, Route } from "react-router-dom";
-import Navbar from "./components/layout/Navbar";
-import Main from "./components/Main";
-import Login from "./components/Login";
-import CreateAccount from "./components/CreateAccount";
-import Movie from "./components/Movie";
+import 'font-awesome/css/font-awesome.min.css';
+import { BrowserRouter as Router, Route } from 'react-router-dom';
+import Navbar from './components/layout/Navbar';
+import Main from './components/Main';
+import Login from './components/Login';
+import CreateAccount from './components/CreateAccount';
+import Movie from './components/Movie';
 class App extends Component {
   constructor(props) {
     super(props);
@@ -16,7 +16,7 @@ class App extends Component {
     };
   }
 
-  setUser = (user) => {
+  setUser = user => {
     this.setState({ user });
   };
 
@@ -26,16 +26,16 @@ class App extends Component {
         <Navbar />
         <Route
           exact
-          path="/"
-          component={(props) => <Main user={this.state.user} />}
+          path='/'
+          component={props => <Main user={this.state.user} />}
         />
         <Route
           exact
-          path="/login"
-          component={(props) => <Login setUser={this.setUser} />}
+          path='/login'
+          component={props => <Login setUser={this.setUser} />}
         />
-        <Route exact path="/createaccount" component={CreateAccount} />
-        <Route exact path="/movie/:id" component={Movie} />
+        <Route exact path='/createaccount' component={CreateAccount} />
+        <Route exact path='/movie/:id' component={Movie} />
       </Router>
     );
   }
--- client/src/components/layout/Navbar.js
+++ client/src/components/layout/Navbar.js
@@ -1,12 +1,12 @@
-import React from "react";
-import { Link } from "react-router-dom";
-import "./Navbar.css";
-const Navbar = (props) => {
+import React from 'react';
+import { Link } from 'react-router-dom';
+import './Navbar.css';
+const Navbar = props => {
   return (
     <div>
       <header>
-        <Link to="" style={{ "text-decoration": "none" }}>
-          <h1 className="header">MyFlicks</h1>
+        <Link to='/' style={{ 'text-decoration': 'none' }}>
+          <h1 className='header'>MyFlicks</h1>
         </Link>
       </header>
     </div>
