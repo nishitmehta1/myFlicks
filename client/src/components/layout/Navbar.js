c0c409853065e00344aa75b8a3ed0884e66c3e77
Nishit Mehta <nishit.mehta1@gmail.com>
2019-07-29 19:02:35 -0500

    Navbar change

--- client/src/components/layout/Navbar.js
+++ client/src/components/layout/Navbar.js
@@ -1,17 +1,47 @@
-import React from "react";
-import { Link } from "react-router-dom";
-import "./Navbar.css";
-const Navbar = (props) => {
+import React from 'react';
+import { Link } from 'react-router-dom';
+import './Navbar.css';
+const Navbar = props => {
   return (
-    <div>
-      <header>
-        <div>
-          <Link to="/" className="header" style={{ textDecoration: "none" }}>
-            MyFlicks
-          </Link>
-        </div>
-      </header>
-    </div>
+    <nav className='navbar navbar-main navbar-expand-lg'>
+      <div>
+        <Link className='navbar-brand brand' href='#'>
+          My Flicks
+        </Link>
+      </div>
+      <div
+        className='collapse navbar-collapse dropdown-main'
+        id='navbarSupportedContent'
+      >
+        <ul className='navbar-nav'>
+          <li className='nav-item dropdown'>
+            <Link
+              className='nav-link dropdown-toggle'
+              href='#'
+              id='navbarDropdown'
+              role='button'
+              data-toggle='dropdown'
+              aria-haspopup='true'
+              aria-expanded='false'
+            >
+              Dropdown
+            </Link>
+            <div class='dropdown-menu' aria-labelledby='navbarDropdown'>
+              <Link class='dropdown-item' href='#'>
+                Action
+              </Link>
+              <Link class='dropdown-item' href='#'>
+                Another action
+              </Link>
+              <div class='dropdown-divider' />
+              <Link class='dropdown-item' href='#'>
+                Something else here
+              </Link>
+            </div>
+          </li>
+        </ul>
+      </div>
+    </nav>
   );
 };
 
